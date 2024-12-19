52c2a24de2cde339d93eb13c3300f4b34950f83f
Yash-89 <yashproraj@gmail.com>
2024-12-19 16:53:09 +0530

    Initial Commit

---
+++ chat/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import HttpResponse, JsonResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'room.html', {
+        'room': room,
+        'username': username,
+        'room_details': room_details,
+    })
+
+def checkview(request):
+    username= request.POST['username']
+    room = request.POST['room_name']
+
+    if Room.objects.filter(name=room).exists() == False:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+
+        return redirect('/' + room + '/?username=' + username)
+    
+    return redirect('/' + room + '/?username=' + username)
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_msg = Message.objects.create(value=message, user=username, room=room_id)
+    new_msg.save()
+
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+    messages = Message.objects.filter(room=room_details.id)
+
+    return JsonResponse({'messages': list(messages.values())})
